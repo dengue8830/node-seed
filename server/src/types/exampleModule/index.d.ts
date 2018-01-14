9a1e0b3427bb9ff9f4e4abd8cceb36bc048d8691
David Rearte <dengue8830@gmail.com>
2018-01-14 10:40:40 -0300

    agregado example module

---
+++ server/src/types/exampleModule/index.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Example of how to create a custom module, for example to create the types
+ * for a third party lib that doesn't have a type in npm.
+ *
+ * 1. Use the "include" and "paths" options in the tsconfig.json of this project
+ * 2. Create a /types folder
+ * 3. Create a folder for your custom module (the same name of the module you want create)
+ * 4. Create a index.d.ts in that folder
+ * 5. Declare the module with the same name of the folder in quotes
+ * 6. Exports your elements
+ * 7. Use your elements like this
+ *
+ * import { SomeClass } from 'exampleModule';
+ */
+
+declare module 'exampleModule' {
+    export class SomeClass { }
+    export interface SomeInterface { }
+}
