7fae16bfd8bd884d243d3de3df2bbeec04797575
David Rearte <dengue8830@gmail.com>
2018-04-08 13:54:19 -0300

    agregagado app.ts como clase

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,21 +7,43 @@ import authService from './components/auth/authService';
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 
-const app: Application = express();
+export default class App {
+    private app: Application;
 
-app.use(helmet({
-    // until we use https
-    hsts: false
-}));
+    async init() {
+        // An array of promises to do the async work
+        const promises: Promise<any>[] = [];
+        this.app = express();
+        // Enable CORS
+        // this.app.use(function (req, res, next) {
+        //     res.header('Access-Control-Allow-Origin', '*'); // specific domains are recomended
+        //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+        //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        //     next();
+        // });
+        this.app.use(helmet({
+            // until we use https
+            hsts: false
+        }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
 
-authService.init(app);
+        authService.init(this.app);
 
-// here routes
-apisRoutesLoader(app);
+        // here routes
+        apisRoutesLoader(this.app);
 
-app.use(errorHandlerMdl);
+        // promises.push(LockFile.borrarTodosLocksFiles()
+        //     .catch(error => {
+        //         console.error('error al borrar lock file', error);
+        //     }));
 
-export default app;
+        this.app.use(errorHandlerMdl);
+        return Promise.all(promises);
+    }
+
+    getExpressApp(): Application {
+        return this.app;
+    }
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,15 @@
-import app from './app';
+import App from './app';
+import logger from './common/logger';
+
+const app = new App();
+app.init();
+const port = process.env.NODE_ENV === 'production' ? 8080 : 8080;
+
+logger.info(`env ${process.env.NODE_ENV}`);
+logger.info(`port ${port}`);
 
 // Splitting this from app.ts we can eg: create multiples servers on diferent ports with the same app, or something else
-const server = app.listen(8080, () => {
-    console.log('running on 8080....');
+const server = app.getExpressApp().listen(port, () => {
+    logger.info('server running...');
 });
 export default server;
