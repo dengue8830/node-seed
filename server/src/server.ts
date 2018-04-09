b71243f47527d92cf714a4d4b26620c5e4e341a0
David Rearte <dengue8830@gmail.com>
2018-04-09 00:22:26 -0300

    agregado comentario

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import logger from './common/logger';
 logger.info(`env ${process.env.NODE_ENV}`);
 const app = new App();
 const port = process.env.NODE_ENV === 'production' ? 8080 : 8080;
+// const port = 808 + process.env.NODE_APP_INSTANCE;
 // Splitting this from app.ts we can eg: create multiples servers on diferent ports with the same app, or something else
 const server = app.getExpressApp().listen(port, () => {
     logger.info(`app server running on port ${port}...`);
