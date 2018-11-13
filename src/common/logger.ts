4a00d428499f0876bf2f3314217f3bbfdf869588
David Eduardo Rearte <dengue8830@gmail.com>
2018-11-13 18:41:28 -0300

    mejorado logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -28,71 +28,84 @@ const customLevels = {
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 
-// const withColorsAndTime = winston.format.combine(
-//   winston.format.colorize(),
-//   winston.format.timestamp(),
-//   winston.format.printf((info) => {
-//     const { timestamp, level, message, ...args } = info;
-//     const ts = moment(timestamp).local().format('HH:MM:ss');
-//     return `${ts} ${level}: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
-//   }),
-// );
+const parser = (param: any): string => {
+  if (!param) {
+    return '';
+  }
+  if (typeof param === 'string') {
+    return param;
+  }
+  return Object.keys(param).length ? JSON.stringify(param, undefined, 2) : '';
+};
+
+const formatter = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.splat(),
+  winston.format.printf((info) => {
+    const { timestamp, level, message, meta } = info;
+    const ts = moment(timestamp).local().format('HH:MM:ss');
+    const metaMsg = meta ? `: ${parser(meta)}` : '';
+    return `${ts} [${level}] ${parser(message)} ${metaMsg}`;
+  }),
+);
 
 class Logger {
   private logger: winston.Logger;
 
   constructor() {
-    // Logs error and fatal
-    const prodTransport = new (winston.transports.File)({ filename: 'logs/error.log', level: 'error' });
-    // Logs debug, info, and forward
-    const devTransport = new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-      // format: withColorsAndTime,
-      level: 'trace'
+    // Use this config for separate transport for prod and dev if you are not using pm2. It handles the console.logs and put them in the corresponding file
+    // const prodTransport = new winston.transports.File({ filename: 'logs/error.log', level: 'error' });
+    const transport = new winston.transports.Console({
+      // format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+      format: formatter
     });
     this.logger = winston.createLogger({
-      level: isProductionEnv ? 'error' : 'debug',
+      // Logs error and fatal for prod and the rest for dev
+      level: isProductionEnv ? 'error' : 'trace',
       // We redefine security leves as mentioned
       levels: customLevels.levels,
-      transports: [isProductionEnv ? prodTransport : devTransport]
+      transports: [transport]
     });
     winston.addColors(customLevels.colors);
   }
 
-  trace(msg: string) {
-    this.logger.log('trace', msg);
+  trace(msg: any, meta?: any) {
+    this.logger.log('trace', msg, meta);
   }
 
-  debug(msg: string) {
-    this.logger.debug(msg);
+  debug(msg: any, meta?: any) {
+    this.logger.debug(msg, meta);
   }
 
-  info(msg: string) {
-    this.logger.info(msg);
+  info(msg: any, meta?: any) {
+    this.logger.info(msg, meta);
   }
 
-  warn(msg: string) {
-    this.logger.warn(msg);
+  warn(msg: any, meta?: any) {
+    this.logger.warn(msg, meta);
   }
 
-  error(msg: string) {
-    this.logger.error(msg);
+  error(msg: any, meta?: any) {
+    this.logger.error(msg, meta);
+    // Use this way to make pm2 put the error log in the error file
+    // Set the name of the error file in start.json
+    // TODO: extract the printf function to reuse the code
+    // console.error(msg, meta);
   }
 
-  fatal(msg: string) {
-    this.logger.log('fatal', msg);
+  fatal(msg: any, meta?: any) {
+    this.logger.log('fatal', msg, meta);
   }
 }
 
 /**
  * Examples
  */
-
-// logger.trace('testing');
-// logger.debug('testing');
-// logger.info('testing');
-// logger.warn('testing');
-// logger.error('testing');
-// logger.fatal('testing');
+// logger.trace({ x: 2 }, { a: 1 });
+// logger.trace('hi', { a: 1 });
+// logger.trace({ x: 2 }, 'by');
+// logger.trace('hi again');
+// logger.trace('that its', 'by');
 
 export const logger = new Logger();
