85efa7b28d0ad60d34f606eab58d83c6e10a3361
David Eduardo Rearte <dengue8830@gmail.com>
2018-11-14 08:52:16 -0300

    actualizado dependencias

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,14 @@ import { logger } from './common/logger';
 // https://github.com/Unitech/pm2/issues/637
 // https://github.com/socketio/socket.io/issues/1942#issuecomment-82352072
 // https://github.com/Unitech/PM2/issues/1510
-// logger.info(`env ${process.env.NODE_ENV}`);
-logger.error('ahora si', 'chau');
+logger.info(`env ${process.env.NODE_ENV}`);
+
 const app = new App();
 const port = process.env.NODE_ENV === 'production' ? 8080 : 8080;
 // const port = 808 + process.env.NODE_APP_INSTANCE;
 // Splitting this from app.ts we can eg: create multiples servers on diferent ports with the same app, or something else
 const server = app.getExpressApp().listen(port, () => {
-  // logger.info(`app server running on port ${port}...`);
+  logger.info(`app server running on port ${port}...`);
   app.init(server);
 });
 
