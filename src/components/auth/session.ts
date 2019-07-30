9392a05803aa7aeccb9d04a6b246a933521b1f06
David Rearte <davidrearte@MacBook-Pro.local>
2019-07-30 11:50:58 -0300

    addes session

---
+++ src/components/auth/session.ts
@@ -0,0 +1,46 @@
+import { Request } from 'express';
+
+export interface ISession {
+  user: {
+    id: string,
+    isRoot: boolean,
+    isGuest: boolean
+  }
+}
+
+export class Session implements ISession {
+  user: {
+    id: string,
+    isRoot: boolean,
+    isGuest: boolean
+  }
+
+  /**
+   * Abstract the way we store the session in the request.
+   * @param req Express Request
+   */
+  static parseRequest(req: Request): Session {
+    const session = new Session();
+    // On this case req.user is type Session
+    session.user = req.user.usuario;
+    return session;
+  }
+
+  /**
+   * Passport "user" is our session, so we use it as session
+   */
+  static parseSession(rawSession: ISession) {
+    const session = new Session();
+    // On this case req.user is type Session
+    session.user = rawSession.user;
+    return session;
+  }
+
+  isRoot(): boolean {
+    return !!this.user.isRoot;
+  }
+
+  isGuest(): boolean {
+    return !!this.user.isGuest;
+  }
+}
