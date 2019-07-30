e18f54f2abd0a4561d9998a618591aa16d2ee686
David Rearte <davidrearte@MacBook-Pro.local>
2019-07-29 21:38:47 -0300

    usando asyncmdl

--- src/components/chatt/chat.apis.ts
+++ src/components/chatt/chat.apis.ts
@@ -1,13 +1,14 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import socket from './socket';
 import { logger } from '../../common/logger';
+import { asyncMdl } from '../../common/async.mdl';
 
 const router = Router();
 
-router.get('/api/chat/v1', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/api/chat/v1', asyncMdl(async (req: Request, res: Response, next: NextFunction) => {
   logger.info(`por api ${req.query.message}`);
   socket.sendMessage(req.query.message);
   res.json({ status: 'ok' });
-});
+}));
 
 export default router;
--- src/components/user/user.apis.ts
+++ src/components/user/user.apis.ts
@@ -8,6 +8,7 @@ import sequelize from '../../common/connection';
 import { Op } from 'sequelize';
 import { Group } from '../group/group.model';
 import { logger } from '../../common/logger';
+import { asyncMdl } from '../../common/async.mdl';
 
 const router = Router();
 
@@ -15,12 +16,12 @@ const router = Router();
  * Client request a token to operate on protected apis.
  * Changes this with your login logic.
  */
-router.get('/api/users/v1', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/api/users/v1', asyncMdl(async (req: Request, res: Response, next: NextFunction) => {
   const users = await User.findAll({ include: [{ all: true }] });
   res.json({ users });
-});
+}));
 
-router.get('/secret/syncforce', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/secret/syncforce', asyncMdl(async (req: Request, res: Response, next: NextFunction) => {
   await sequelize.sync({ force: true });
   await User.create({ email: 'xxx' });
   const user = await User.findOne({
@@ -41,6 +42,6 @@ router.get('/secret/syncforce', async (req: Request, res: Response, next: NextFu
   logger.debug(`cantidad ${users.length}`);
   // await user.destroy();
   res.json({ status: 'ok' });
-});
+}));
 
 export default router;
