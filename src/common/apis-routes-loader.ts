5e78f5155f657cb9f4c3e77abdc510c35112f386
David Rearte <davidrearte@MacBook-Pro.local>
2019-07-31 16:03:26 -0300

    fix

--- src/common/apis-routes-loader.ts
+++ src/common/apis-routes-loader.ts
@@ -2,7 +2,6 @@ import { CommonUtil } from './utils/common.util';
 import { devApis } from './utils/dev.apis';
 import { Application } from 'express';
 import { authApis } from '../components/auth/auth.apis';
-import { chatApis } from '../components/chatt/chat.apis';
 import { testApis } from './utils/test.apis';
 
 /**
@@ -10,7 +9,6 @@ import { testApis } from './utils/test.apis';
  */
 export function apisRoutesLoader(app: Application): void {
   app.use(authApis);
-  app.use(chatApis);
   if (CommonUtil.isDev) {
     app.use(devApis);
   }
